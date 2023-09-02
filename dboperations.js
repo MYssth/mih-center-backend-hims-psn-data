ed7142f7351f17ba0ea757f312ba25cc43dcb9ce
pongsit-po <pongsit.po@outlook.com>
2023-09-02 09:44:01 +0700

    update v1.3  - add getalldept function

--- dboperations.js
+++ dboperations.js
@@ -1,167 +1,229 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
-var config = require('./dbconfig');
-const sql = require('mssql');
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+var config = require("./dbconfig");
+const sql = require("mssql");
 
 async function psnSpcClr(rData) {
-    if (rData.length) {
-        for (let i = 0; i < rData.length; i += 1) {
-            await Object.assign(rData[i], {
-                "psn_id": (rData[i].psn_id).split(" ").join(""),
-                "pname": (rData[i].pname).split(" ").join(""),
-                "fname": (rData[i].fname).split(" ").join(""),
-                "lname": (rData[i].lname).split(" ").join(""),
-                "bdate": (rData[i].bdate).split(" ").join(""),
-                "fld_id": (rData[i].fld_id).split(" ").join(""),
-                "fld_name": (rData[i].fld_name).split(" ").join(""),
-                "fac_id": (rData[i].fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join(""),
-                "fac_name": (rData[i].fac_name).split(" ").join(""),
-                "dept_id": (rData[i].fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join("") + "" + (rData[i].dept_id).split(" ").join(""),
-                "dept_name": (rData[i].dept_name).split(" ").join(""),
-                "pos_id": (rData[i].pos_id).split(" ").join(""),
-                "pos_name": (rData[i].pos_name).split(" ").join(""),
-            });
-        }
+  if (rData.length) {
+    for (let i = 0; i < rData.length; i += 1) {
+      await Object.assign(rData[i], {
+        psn_id: rData[i].psn_id.split(" ").join(""),
+        pname: rData[i].pname.split(" ").join(""),
+        fname: rData[i].fname.split(" ").join(""),
+        lname: rData[i].lname.split(" ").join(""),
+        bdate: rData[i].bdate.split(" ").join(""),
+        fld_id: rData[i].fld_id?.split(" ").join(""),
+        fld_name: rData[i].fld_name?.split(" ").join(""),
+        fac_id:
+          rData[i].fld_id?.split(" ").join("") +
+          "" +
+          rData[i].fac_id.split(" ").join(""),
+        fac_name: rData[i].fac_name?.split(" ").join(""),
+        dept_id:
+          rData[i].fld_id?.split(" ").join("") +
+          "" +
+          rData[i].fac_id.split(" ").join("") +
+          "" +
+          rData[i].dept_id.split(" ").join(""),
+        dept_name: rData[i].dept_name?.split(" ").join(""),
+        pos_id: rData[i].pos_id?.split(" ").join(""),
+        pos_name: rData[i].pos_name?.split(" ").join(""),
+      });
     }
-    else {
-        await Object.assign(rData, {
-            "psn_id": (rData.psn_id).split(" ").join(""),
-            "pname": (rData.pname).split(" ").join(""),
-            "fname": (rData.fname).split(" ").join(""),
-            "lname": (rData.lname).split(" ").join(""),
-            "bdate": (rData.bdate).split(" ").join(""),
-            "fld_id": (rData.fld_id).split(" ").join(""),
-            "fld_name": (rData.fld_name).split(" ").join(""),
-            "fac_id": (rData.fld_id).split(" ").join("") + "" + (rData.fac_id).split(" ").join(""),
-            "fac_name": (rData.fac_name).split(" ").join(""),
-            "dept_id": (rData.fld_id).split(" ").join("") + "" + (rData.fac_id).split(" ").join("") + "" + (rData.dept_id).split(" ").join(""),
-            "dept_name": (rData.dept_name).split(" ").join(""),
-            "pos_id": (rData.pos_id).split(" ").join(""),
-            "pos_name": (rData.pos_name).split(" ").join(""),
-        });
-    }
-    return rData;
+  } else {
+    await Object.assign(rData, {
+      psn_id: rData.psn_id.split(" ").join(""),
+      pname: rData.pname.split(" ").join(""),
+      fname: rData.fname.split(" ").join(""),
+      lname: rData.lname.split(" ").join(""),
+      bdate: rData.bdate.split(" ").join(""),
+      fld_id: rData.fld_id?.split(" ").join(""),
+      fld_name: rData.fld_name?.split(" ").join(""),
+      fac_id:
+        rData.fld_id?.split(" ").join("") +
+        "" +
+        rData.fac_id.split(" ").join(""),
+      fac_name: rData.fac_name?.split(" ").join(""),
+      dept_id:
+        rData.fld_id?.split(" ").join("") +
+        "" +
+        rData.fac_id.split(" ").join("") +
+        "" +
+        rData.dept_id.split(" ").join(""),
+      dept_name: rData.dept_name?.split(" ").join(""),
+      pos_id: rData.pos_id?.split(" ").join(""),
+      pos_name: rData.pos_name?.split(" ").join(""),
+    });
+  }
+  return rData;
 }
 
-const psnQryTxt = "SELECT" +
-    " STFNO AS psn_id" +
-    ", PRENAM AS pname" +
-    ", NAM AS fname" +
-    ", SURNAM AS lname" +
-    ", HBD as bdate" +
-    ", VIEW_STFMAS.รหัสสายงาน AS fld_id" +
-    ", VIEW_DEPT.ชื่อสายงาน AS fld_name" +
-    ", VIEW_STFMAS.รหัสฝ่ายงาน AS fac_id" +
-    ", VIEW_DEPT.ชื่อฝ่ายงาน AS fac_name" +
-    ", VIEW_STFMAS.รหัสหน่วยงาน AS dept_id" +
-    ", VIEW_DEPT.ชื่อหน่วยงาน AS dept_name" +
-    ", VIEW_STFMAS.รหัสตำแหน่ง AS pos_id" +
-    ", VIEW_JOB_POSITION.ชื่อตำแหน่ง AS pos_name" +
-    " FROM VIEW_STFMAS" +
-    " LEFT JOIN VIEW_DEPT" +
-    " ON ( VIEW_DEPT.รหัสสายงาน = VIEW_STFMAS.รหัสสายงาน AND VIEW_DEPT.รหัสฝ่ายงาน = VIEW_STFMAS.รหัสฝ่ายงาน AND VIEW_DEPT.รหัสหน่วยงาน = VIEW_STFMAS.รหัสหน่วยงาน )" +
-    " LEFT JOIN VIEW_JOB_POSITION" +
-    " ON VIEW_JOB_POSITION.รหัสตำแหน่ง = VIEW_STFMAS.รหัสตำแหน่ง" +
-    " WHERE ( VIEW_STFMAS.รหัสสายงาน <> '' AND VIEW_STFMAS.รหัสฝ่ายงาน <> '' AND VIEW_STFMAS.รหัสหน่วยงาน <> '' ) ";
+const psnQryTxt =
+  "SELECT" +
+  " STFNO AS psn_id" +
+  ", PRENAM AS pname" +
+  ", NAM AS fname" +
+  ", SURNAM AS lname" +
+  ", HBD as bdate" +
+  ", VIEW_STFMAS.รหัสสายงาน AS fld_id" +
+  ", VIEW_DEPT.ชื่อสายงาน AS fld_name" +
+  ", VIEW_STFMAS.รหัสฝ่ายงาน AS fac_id" +
+  ", VIEW_DEPT.ชื่อฝ่ายงาน AS fac_name" +
+  ", VIEW_STFMAS.รหัสหน่วยงาน AS dept_id" +
+  ", VIEW_DEPT.ชื่อหน่วยงาน AS dept_name" +
+  ", VIEW_STFMAS.รหัสตำแหน่ง AS pos_id" +
+  ", VIEW_JOB_POSITION.ชื่อตำแหน่ง AS pos_name" +
+  ", VIEW_STFMAS.วันที่ลาออก AS quit_date" +
+  " FROM VIEW_STFMAS" +
+  " LEFT JOIN VIEW_DEPT" +
+  " ON ( VIEW_DEPT.รหัสสายงาน = VIEW_STFMAS.รหัสสายงาน AND VIEW_DEPT.รหัสฝ่ายงาน = VIEW_STFMAS.รหัสฝ่ายงาน AND VIEW_DEPT.รหัสหน่วยงาน = VIEW_STFMAS.รหัสหน่วยงาน )" +
+  " LEFT JOIN VIEW_JOB_POSITION" +
+  " ON VIEW_JOB_POSITION.รหัสตำแหน่ง = VIEW_STFMAS.รหัสตำแหน่ง" +
+  " WHERE ( VIEW_STFMAS.รหัสสายงาน <> '' AND VIEW_STFMAS.รหัสฝ่ายงาน <> '' AND VIEW_STFMAS.รหัสหน่วยงาน <> '' ) ";
 
-async function getAllPSNData() {
-    try {
-        console.log("getAllPSNData call try connect to server");
-        let pool = await sql.connect(config);
-        console.log("connect complete");
-        const queryData = await pool.request().query(psnQryTxt);
-        let result = queryData.recordsets[0]
-        if (result) {
-            result = await psnSpcClr(queryData.recordsets[0]);
-        }
-        console.log("getAllPSNData complete");
-        console.log("====================");
-        return result;
+async function getAllPSN() {
+  try {
+    console.log("getAllPSN call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const queryData = await pool.request().query(psnQryTxt);
+    let result = queryData.recordsets[0];
+    if (result) {
+      result = await psnSpcClr(queryData.recordsets[0]);
     }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
+    console.log("getAllPSN complete");
+    console.log("====================");
+    return result;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function getActvPSN() {
+  try {
+    console.log("getActvPSN call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const queryData = await pool
+      .request()
+      .query(psnQryTxt + "AND VIEW_STFMAS.วันที่ลาออก = '00000'");
+    let result = queryData.recordsets[0];
+    if (result) {
+      result = await psnSpcClr(queryData.recordsets[0]);
     }
+    console.log("getActvPSN complete");
+    console.log("====================");
+    return result;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 async function getPSNDataById(STFNO) {
-    try {
-        console.log("getPSNDataById call try connect to server");
-        let pool = await sql.connect(config);
-        console.log("connect complete");
-        const queryData = await pool.request()
-            .input("STFNO", sql.VarChar, STFNO)
-            .query(psnQryTxt + "AND STFNO = @STFNO");
-        let result = queryData.recordset[0];
-        if (result) {
-            result = await psnSpcClr(queryData.recordset[0]);
-        }
-        else {
-            result = {};
-        }
-        console.log("getPSNDataById complete");
-        console.log("====================");
-        return result;
+  try {
+    console.log("getPSNDataById call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const queryData = await pool
+      .request()
+      .input("STFNO", sql.VarChar, STFNO)
+      .query(psnQryTxt + "AND STFNO = @STFNO");
+    let result = queryData.recordset[0];
+    if (result) {
+      result = await psnSpcClr(queryData.recordset[0]);
+    } else {
+      result = {};
     }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
-    }
+    console.log("getPSNDataById complete");
+    console.log("====================");
+    return result;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 async function deptSpcClr(rData) {
-    if (rData.length) {
-        for (let i = 0; i < rData.length; i += 1) {
-            await Object.assign(rData[i], {
-                "fld_id": (rData[i].fld_id).split(" ").join(""),
-                "fld_name": (rData[i].fld_name).split(" ").join(""),
-                "fac_id": (rData[i].fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join(""),
-                "fac_name": (rData[i].fac_name).split(" ").join(""),
-                "dept_id": (rData[i].fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join("") + "" + (rData[i].dept_id).split(" ").join(""),
-                "dept_name": (rData[i].dept_name).split(" ").join(""),
-            });
-        }
+  if (rData.length) {
+    for (let i = 0; i < rData.length; i += 1) {
+      await Object.assign(rData[i], {
+        fld_id: rData[i].fld_id.split(" ").join(""),
+        fld_name: rData[i].fld_name.split(" ").join(""),
+        fac_id:
+          rData[i].fld_id.split(" ").join("") +
+          "" +
+          rData[i].fac_id.split(" ").join(""),
+        fac_name: rData[i].fac_name.split(" ").join(""),
+        dept_id:
+          rData[i].fld_id.split(" ").join("") +
+          "" +
+          rData[i].fac_id.split(" ").join("") +
+          "" +
+          rData[i].dept_id.split(" ").join(""),
+        dept_name: rData[i].dept_name.split(" ").join(""),
+      });
     }
-    else {
-        await Object.assign(rData, {
-            "fld_id": (rData.fld_id).split(" ").join(""),
-            "fld_name": (rData.fld_name).split(" ").join(""),
-            "fac_id": (rData.fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join(""),
-            "fac_name": (rData.fac_name).split(" ").join(""),
-            "dept_id": (rData.fld_id).split(" ").join("") + "" + (rData[i].fac_id).split(" ").join("") + "" + (rData[i].dept_id).split(" ").join(""),
-            "dept_name": (rData.dept_name).split(" ").join(""),
-        });
-    }
-    return rData;
+  } else {
+    await Object.assign(rData, {
+      fld_id: rData.fld_id.split(" ").join(""),
+      fld_name: rData.fld_name.split(" ").join(""),
+      fac_id:
+        rData.fld_id.split(" ").join("") +
+        "" +
+        rData[i].fac_id.split(" ").join(""),
+      fac_name: rData.fac_name.split(" ").join(""),
+      dept_id:
+        rData.fld_id.split(" ").join("") +
+        "" +
+        rData[i].fac_id.split(" ").join("") +
+        "" +
+        rData[i].dept_id.split(" ").join(""),
+      dept_name: rData.dept_name.split(" ").join(""),
+    });
+  }
+  return rData;
 }
 
-const deptQryTxt = "SELECT" +
-    " รหัสสายงาน AS fld_id" +
-    ", ชื่อสายงาน AS fld_name" +
-    ", รหัสฝ่ายงาน AS fac_id" +
-    ", ชื่อฝ่ายงาน AS fac_name" +
-    ", รหัสหน่วยงาน AS dept_id" +
-    ", ชื่อหน่วยงาน AS dept_name" +
-    " FROM VIEW_DEPT WHERE รหัสหน่วยงาน <> ''";
+const deptQryTxt =
+  "SELECT" +
+  " รหัสสายงาน AS fld_id" +
+  ", ชื่อสายงาน AS fld_name" +
+  ", รหัสฝ่ายงาน AS fac_id" +
+  ", ชื่อฝ่ายงาน AS fac_name" +
+  ", รหัสหน่วยงาน AS dept_id" +
+  ", ชื่อหน่วยงาน AS dept_name" +
+  " FROM VIEW_DEPT WHERE รหัสหน่วยงาน <> ''";
 
 async function getAllDept() {
-    try {
-        console.log("getAllDept call try connect to server");
-        let pool = await sql.connect(config);
-        console.log("connect complete");
-        const queryData = await pool.request().query(deptQryTxt);
-        let result = deptSpcClr(queryData.recordsets[0]);
-        console.log("getAllDept complete");
-        console.log("====================");
-        return result;
-    }
-    catch (error) {
-        console.error(error);
-        return { "status": "error", "message": error.message };
-    }
+  try {
+    console.log("getAllDept call try connect to server");
+    let pool = await sql.connect(config);
+    console.log("connect complete");
+    const queryData = await pool.request().query(deptQryTxt);
+    let result = deptSpcClr(queryData.recordsets[0]);
+    console.log("getAllDept complete");
+    console.log("====================");
+    return result;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
+}
+
+async function getVersion() {
+  try {
+    return process.env.version;
+  } catch (error) {
+    console.error(error);
+    return { status: "error", message: error.message };
+  }
 }
 
 module.exports = {
-    getAllPSNData: getAllPSNData,
-    getPSNDataById: getPSNDataById,
-    getAllDept: getAllDept,
-}
+  getAllPSN: getAllPSN,
+  getActvPSN: getActvPSN,
+  getPSNDataById: getPSNDataById,
+  getAllDept: getAllDept,
+  getVersion: getVersion,
+};
--- server.js
+++ server.js
@@ -1,69 +1,98 @@
-const dboperations = require('./dboperations');
+const dboperations = require("./dboperations");
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-const { request, response } = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+const { request, response } = require("express");
 var app = express();
 var router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: '*'
-}));
-app.use('/api/himspsn', router);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use("/api/himspsn", router);
 
 router.use((request, response, next) => {
-    //write authen here
+  //write authen here
 
-    response.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  response.setHeader("Access-Control-Allow-Credentials", true);
 
-    // console.log('middleware');
-    next();
+  // console.log('middleware');
+  next();
 });
 
 router.route("/health").get((request, response) => {
-    // console.log("health check");
-    response.json({ status: 200 });
-  });
-
-router.route('/getallpsndata').get((request, response) => {
-
-    dboperations.getAllPSNData().then(result => {
-        response.status(201).json(result);
-    }).catch(err => {
-        console.error(err);
-        response.setStatus(500);
-    });
-
+  // console.log("health check");
+  response.json({ status: 200 });
 });
 
-router.route('/getpsndatabyid/:stfno').get((request, response) => {
-
-    dboperations.getPSNDataById(request.params.stfno).then(result => {
-        response.status(201).json(result);
-    }).catch(err => {
-        console.error(err);
-        response.setStatus(500);
+router.route("/getallpsn").get((request, response) => {
+  dboperations
+    .getAllPSN()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
     });
-
 });
 
-router.route('/getalldept').get((request, response) => {
-
-    dboperations.getAllDept().then(result => {
-        response.status(201).json(result);
-    }).catch(err => {
-        console.error(err);
-        response.setStatus(500);
+router.route("/getactvpsn").get((request, response) => {
+  dboperations
+    .getActvPSN()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
     });
+});
 
+router.route("/getpsndatabyid/:stfno").get((request, response) => {
+  dboperations
+    .getPSNDataById(request.params.stfno)
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
+    });
+});
+
+router.route("/getalldept").get((request, response) => {
+  dboperations
+    .getAllDept()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
+    });
+});
+
+router.route("/getversion").get((request, response) => {
+  dboperations
+    .getVersion()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.setStatus(500);
+    });
 });
 
 var port = process.env.PORT;
 app.listen(port);
-console.log('hims-psn-data API is running at ' + port);
+console.log("hims-psn-data API is running at " + port);
